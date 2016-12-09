a6ca89ec92bfaf3a625117bdde44f7ae21dca6ef
jinht <jinjob@icloud.com.com>
2016-12-09 09:30:34 +0800

    关闭SDK内部打印

--- JhtNewsChannelSDK/JhtNewsChannelSDK.h
+++ JhtNewsChannelSDK/JhtNewsChannelSDK.h
@@ -2,6 +2,9 @@
 //  JhtNewsChannelSDK.h
 //  JhtNewsChannelSDK
 //
+//  GitHub主页: https://github.com/jinht
+//  CSDN博客: http://blog.csdn.net/anticipate91
+//
 //  Created by Jht on 16/8/29.
 //  Copyright © 2016年 Jht. All rights reserved.
 //
